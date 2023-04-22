0c24d44f05e38c3597c4de5f9ea328fc17fdef19
Cedric <cf@eg0.me>
2023-04-22 09:41:02 +0200

    missing thumbs

--- medias/js/script.js
+++ medias/js/script.js
@@ -1,52 +1,57 @@
-(function() {
-
-  $(function() {
+(function () {
+  $(function () {
     var $container;
     $(".texte_article").fitVids({
-      customSelector: "iframe[src^='http://www.dailymotion.com']"
+      customSelector: "iframe[src^='//www.dailymotion.com']",
     });
     if (window.innerWidth > 900) {
-      $('img.full').each(function() {
+      $("img.full").each(function () {
         var urlBig;
-        urlBig = $(this).data('big');
+        urlBig = $(this).data("big");
         return $(this).attr("src", urlBig);
       });
     }
-    $(".collapse").each(function() {
-      return $('button[data-target="' + $(this).attr('id') + '"]').on('click', function() {
-        return $('#' + $(this).data('target')).toggleClass('in');
-      });
+    $(".collapse").each(function () {
+      return $('button[data-target="' + $(this).attr("id") + '"]').on(
+        "click",
+        function () {
+          return $("#" + $(this).data("target")).toggleClass("in");
+        }
+      );
     });
-    if ($('.camera_wrap').length !== 0) {
-      $('.camera_wrap').camera({
-        fx: 'simpleFade',
-        height: '61%',
-        loaderOpacity: .4,
+    if ($(".camera_wrap").length !== 0) {
+      $(".camera_wrap").camera({
+        fx: "simpleFade",
+        height: "61%",
+        loaderOpacity: 0.4,
         overlayer: false,
         mobileAutoAdvance: false,
-        thumbnails: true
+        thumbnails: true,
       });
     }
-    $container = $('.masonry');
-    if ($('#programme').length) {
-      $(window).bind('scrollstop ', function() {
+    $container = $(".masonry");
+    if ($("#programme").length) {
+      $(window).bind("scrollstop ", function () {
         if ($(window).innerWidth() > 800) {
-          return $('#fixed').animate({
-            marginTop: $(window).scrollTop()
-          }, 600, 'easeOutQuad');
+          return $("#fixed").animate(
+            {
+              marginTop: $(window).scrollTop(),
+            },
+            600,
+            "easeOutQuad"
+          );
         } else {
-          return $('#fixed').css('marginTop', 0);
+          return $("#fixed").css("marginTop", 0);
         }
       });
     }
-    return $container.imagesLoaded(function() {
+    return $container.imagesLoaded(function () {
       return $container.masonry({
-        itemSelector: '.box',
-        columnWidth: function(containerWidth) {
+        itemSelector: ".box",
+        columnWidth: function (containerWidth) {
           return containerWidth / 3;
-        }
+        },
       });
     });
   });
-
 }).call(this);
